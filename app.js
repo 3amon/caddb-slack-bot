0eac6931ecc7584ebdb1324bde7e7f0b0f1983d0
Eamon White <white.eamon@gmail.com>
2016-01-16 21:28:03 -0600

    Refactor to handler framework for bots

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var config = require('./config.json');
 var carddb = require('./lib/carddb');
 var cardmath = require('./lib/cardmathlib');
 
-slack = new Slack(config.SLACK_BOT_TOKEN, true, true);
+var slack = new Slack(config.SLACK_BOT_TOKEN, true, true);
+
+var msgHandlers = [carddb, cardmath];
 
 slack.on('open', function()
 {
@@ -14,37 +16,19 @@ slack.on('message', function(message)
 { 
     if (message.type = 'message' && message.text)
     {
+        var post = false;
         var response = {};
 
-        if(message.text.startsWith('draw'))
+        for(var i = 0; i < msgHandlers.length && !post; ++i)
         {
-            response.text = cardmath.buildHypergeometricMessage(message);
-            response.post = true;
-        }
-        else
-        {
-            var fuzzynames = carddb.parseMessage(message.text.toLowerCase());
-            if (fuzzynames)
+            if(msgHandlers[i].canHandle(message.text))
             {
-                for (var i = 0; i < fuzzynames.length; ++i) {
-                    var fuzzyname = fuzzynames[i];
-                    var result = carddb.findBestNameMatch(fuzzyname);
-                    var card = result.card;
-                    var exactmatch = result.exactmatch;
-                    if (card) {
-                        response.attachments = [];
-                        var post = carddb.buildPost(card, exactmatch);
-                        response.attachments = [post];
-                    }
-                    else {
-                        response.text = fuzzyname + " not found!";
-                    }
-                    response.post = true;
-                }
+                response = msgHandlers[i].buildPost(message.text);
+                post = true;
             }
         }
 
-        if (response.post) {
+        if (post) {
             var channel = slack.getChannelGroupOrDMByID(message.channel);
             response.username = config.BOT_USERNAME;
             response.icon_emoji = config.BOT_EMOJI;
